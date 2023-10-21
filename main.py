8dbfec734a0b94f76640767d626163718d2e2c37
afdalabdallah <afdalabdallahm@gmail.com>
2023-10-21 19:51:07 +0700

    Fix main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 from flask import Flask, request, jsonify
 import GA
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
@@ -17,5 +17,5 @@ def generate_schedule():
     return object.run()
 
 
-if _name_ == "_main_":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
